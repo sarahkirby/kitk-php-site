d524a48d1e0f71d7e6b4d70576a44e787dc2b1ca
sarahkirby <sk.irby@icloud.com>
2015-12-18 11:18:41 +1300

    CSS register page! Not quite finished, need to make responsive.

--- templates/register.inc.php
+++ templates/register.inc.php
@@ -3,43 +3,54 @@
 $errors = $user->errors; 
 
 ?>
+<div class="container-fluid reg-page">
+<div class="container reg-form">
+  <h1>Sign up</h1>
 
-<h1>Sign up</h1>
+  <form id="registerNewUser" action="?page=auth.store" method="POST">
 
-<form id="registerNewUser" action="?page=auth.store" method="POST">
-
-<!-- User Name -->
-    <div class="form-group <?php if($errors['username']): ?> has-error <?php endif; ?>">
-      <label for="username" class="control-label">User Name</label>
-      <input class="form-control" id="username" name="username" value="<?php echo $user->username; ?>">
+  <!-- User Name -->
+    <div class="row">
+      <div class="form-group <?php if($errors['username']): ?> has-error <?php endif; ?>">
+        <label for="username" class="control-label">User Name</label>
+        <input class="form-control" id="username" name="username" value="<?php echo $user->username; ?>">
+      </div>
     </div>
 
-<!-- Email -->
-    <div class="form-group <?php if($errors['email']): ?> has-error <?php endif; ?>">
-      <label for="email" class="control-label">Email Address</label>
-      <input class="form-control" id="email" name="email" placeholder="example@example.com" value="<?php echo $user->email; ?>">
-      <div class="help-block"><?php echo $errors['email']; ?></div>
+  <!-- Email -->
+    <div class="row">
+      <div class="form-group <?php if($errors['email']): ?> has-error <?php endif; ?>">
+        <label for="email" class="control-label">Email Address</label>
+        <input class="form-control" id="email" name="email" placeholder="example@example.com" value="<?php echo $user->email; ?>">
+        <div class="help-block"><?php echo $errors['email']; ?></div>
+      </div>
     </div>
 
-<!-- Password -->
-    <div class="form-group <?php if($errors['password']): ?> has-error <?php endif; ?>">
-      <label for="password" class="control-label">Password</label>
-        <input type="password" class="form-control" id="password" name="password">
-        <div class="help-block"><?php echo $errors['password']; ?></div>
+  <!-- Password -->
+    <div class="row">
+      <div class="form-group <?php if($errors['password']): ?> has-error <?php endif; ?>">
+        <label for="password" class="control-label">Password</label>
+          <input type="password" class="form-control" id="password" name="password">
+          <div class="help-block"><?php echo $errors['password']; ?></div>
+      </div>
     </div>
 
-<!-- Password match -->
+  <!-- Password match -->
 
-    <div class="form-group <?php if($errors['password2']): ?> has-error <?php endif; ?>">
-      <label for="password2" class="control-label">Confirm Password</label>
-        <input type="password" class="form-control" id="password2" name="password2">
-        <div class="help-block"><?php echo $errors['password2']; ?></div>
+    <div class="row">
+      <div class="form-group <?php if($errors['password2']): ?> has-error <?php endif; ?>">
+        <label for="password2" class="control-label">Confirm Password</label>
+          <input type="password" class="form-control" id="password2" name="password2">
+          <div class="help-block"><?php echo $errors['password2']; ?></div>
+      </div>
     </div>
 
-    <div class="form-group">
-        <button class="btn btn-success"><span class="glyphicon glyphicon-ok"></span> Sign up</button>
-    </div>
-</form>
+      <div class="form-group">
+          <button class="btn btn-success"><span class="glyphicon glyphicon-ok"></span> Sign up</button>
+      </div>
+  </form>
+</div>
+</div>
 
 
 
