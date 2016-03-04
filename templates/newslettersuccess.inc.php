cdd780ecde2fb29c6bc32696439c0067edb7dd98
sarahkirby <sk.irby@icloud.com>
2016-03-04 23:40:05 +1300

    removed success code that didn't work for both forms.

--- templates/newslettersuccess.inc.php
+++ templates/newslettersuccess.inc.php
@@ -1,17 +1,3 @@
-<?php
-	if($_POST['reciperequest']){
-
-	  	$insertHeaderContent = "your recipe suggestion!";
-	  	$pContent = "Keep an eye out as Katie will recreate your recipe very soon!";
-
-	} else {
-
-  		$insertHeaderContent = "signing up to our newsletter!";
-  		$pContent = "Newsletter's will be emailed once a month.";
-
-	}
-?>
-
 <div class="container">
 	<div class="row">
 		<div class="col-xs-12">
